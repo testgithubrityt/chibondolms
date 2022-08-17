6935b4b926aefcb74fb14c606f598d060172b0c9
testgithubrityt <111460661+testgithubrityt@users.noreply.github.com>
2022-08-17 13:00:31 -0700

    Add files via upload

---
+++ title_index.php
@@ -0,0 +1,38 @@
+				
+								<div class="row-fluid">
+
+						<div class="span12">
+						
+						</div>	
+													
+							</div>
+							<div class="row-fluid">
+
+						<div class="span4">
+						<img class="index_logo" src="admin/images/lmsa.png">
+						</div>	
+						<div class="span8">
+						
+								<div class="title">
+							<p class="chmsc"><h1>Chibondo Secondary School</h1></p>
+							<h3>
+
+							<p>Online - Learning System</p>
+						
+							</h3>		
+						</div>
+			
+						</div>							
+							</div>
+				
+				<div class="row-fluid">
+
+						<div class="span12">
+						<br>
+								<div class="motto">
+												<p>CHIBONDO EXCELS:</p>
+												<p>Excellence, Competence and Educational</p>
+												<p>Leadership in Science and Technology</p>
+								</div>		
+						</div>		
+				</div>
